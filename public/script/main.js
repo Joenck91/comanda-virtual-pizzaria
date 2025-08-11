5c5a59da35afa844a887592f7ce35bbe2dbeeed1
luisJoenck <copydoluis@gmail.com>
2025-08-11 17:29:19 -0300

    Adicionado menu.json que tem os dados do cardápio. Na pasta interface, criado o view-menu.js responsável pela estruturação do cardápio em html, que é chamado no main.js através de fetch conectando com o menu.json. Estilização do cardápio com principios smacss. E adicionado comentários faltantes no css.

--- public/script/main.js
+++ public/script/main.js
@@ -1,4 +1,22 @@
 import totalFixed from "./interface/total-fixed.js";
+import viewMenuBox from "./interface/view-menu.js";
 
 //Função que fixe a etiqueta total no topo da tela
 totalFixed();
+
+//Funções que precisam acessar json
+document.addEventListener('DOMContentLoaded', ()=>{
+    // Faz uma requisição GET para a rota /api/menu para buscar os dados do cardápio
+    fetch('./data/menu.json')
+    .then(res => res.json()) // Converte a resposta para JSON
+    .then(menu => {
+
+        viewMenuBox(menu);   //Carrega menu
+       
+  
+    })
+    .catch(err => console.error('Erro ao carregar menu:', err));
+
+
+
+});
